6bb58ac5008a09c18c13af5e274c85e75a2c9259
Chaitya <chaitanyaasole@gmail.com>
2024-06-25 12:59:25 +0530

    Made a container for showing to do lists

--- Projects/2 Todo/todo.js
+++ Projects/2 Todo/todo.js
@@ -1,6 +1,33 @@
+let todoList = [];
+//whenver page will be reloaded all lists will be displayed
+displayItems();
+
 function addTodo()
 {
     let inputElement = document.querySelector('#todo-input');
+
     let todoItem = inputElement.value;
-    console.log(todoItem);
+
+    todoList.push(todoItem);
+    inputElement.value = '';
+    displayItems();
+}
+
+function displayItems()
+{
+    let conatinerElement = document.querySelector('.todo-container');
+
+    let newhtml = ``;
+    for(let i = 0; i < todoList.length; i++)
+    {
+        newhtml += `
+        <div>
+        <span>${todoList[i]}</span>
+        <button onclick="todoList.splice(${i},${1});
+        displayItems()">Delete</button>
+        </div>
+        `;
+    }
+
+    conatinerElement.innerHTML = newhtml;
 }
