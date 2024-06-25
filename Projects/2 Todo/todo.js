44b4c4d763d4221cdc37f7fb3051c5c60a6dcded
Chaitya <chaitanyaasole@gmail.com>
2024-06-25 13:25:30 +0530

    Add date field and made list a column

--- Projects/2 Todo/todo.js
+++ Projects/2 Todo/todo.js
@@ -5,11 +5,14 @@ displayItems();
 function addTodo()
 {
     let inputElement = document.querySelector('#todo-input');
+    let inputDate = document.querySelector('#todo-date');
 
     let todoItem = inputElement.value;
+    let todoDate = inputDate.value;
 
-    todoList.push(todoItem);
+    todoList.push({item : todoItem, dueDate: todoDate});
     inputElement.value = '';
+    inputDate.value = '';
     displayItems();
 }
 
@@ -20,9 +23,11 @@ function displayItems()
     let newhtml = ``;
     for(let i = 0; i < todoList.length; i++)
     {
+        let {item, dueDate} = todoList[i];
         newhtml += `
         <div>
-        <span>${todoList[i]}</span>
+        <span>${item}</span>
+        <span>${dueDate}</span>
         <button onclick="todoList.splice(${i},${1});
         displayItems()">Delete</button>
         </div>
