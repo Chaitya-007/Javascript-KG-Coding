a1b80746f46c129421ccf9f02d7a367c1f0cccf4
Chaitya <chaitanyaasole@gmail.com>
2024-07-03 10:44:51 +0530

    Made array and function for displaying items

--- Projects/Myntra_Functional_Clone/scripts/index.js
+++ Projects/Myntra_Functional_Clone/scripts/index.js
@@ -1,4 +1,6 @@
-let itemsContainerElement = document.querySelector('.items-container');
+displayItemsOnHomePage();
+let bagItems = [];
+
 
 /*
 let item = {
@@ -6,33 +8,42 @@ let item = {
     rating: {
         stars: 4.5,
         noOfReviews: 1400,
-    },
-    company_name: 'Carlton London',
-    item_name: 'Rhodium-Plated CZ Floral Studs',
-    current_price: 606,
-    original_price: 1045,
-    discount_percentage: 42,
-};
-*/
+        },
+        company_name: 'Carlton London',
+        item_name: 'Rhodium-Plated CZ Floral Studs',
+        current_price: 606,
+        original_price: 1045,
+        discount_percentage: 42,
+        };
+        */
+       
+       function addToBag(itemId) {
+           bagItems.push(itemId);
+        }
+        
+function displayItemsOnHomePage() {
+    let itemsContainerElement = document.querySelector('.items-container');
+            
 
-let innerHTML = '';
-items.forEach(item => {
-    innerHTML += `
-    <div class="item-container">
-        <img class="item-image" src="${item.item_image}" alt="item image">
-        <div class="rating">
-           ${item.rating.stars} ⭐| ${item.rating.noOfReviews}
-       </div>
-       <div class="company-name">${item.company_name}</div>
-       <div class="item-name">${item.item_name}/div>
-        <div class="price">
-           <span class="current-price">Rs ${item.current_price}</span>
-           <span class="original-price">Rs ${item.original_price}</span>
-           <span class="discount">(${item.discount_percentage}% OFF)</span>
-       </div>
-           <button class="btn-add-bag">Add To Bag</button>
-    </div>
-    
-    `;
-});
-itemsContainerElement.innerHTML = innerHTML;
+    let innerHTML = '';
+    items.forEach(item => {
+        innerHTML += `
+        <div class="item-container">
+            <img class="item-image" src="${item.image}" alt="item image">
+            <div class="rating">
+               ${item.rating.stars} ⭐| ${item.rating.count}
+           </div>
+           <div class="company-name">${item.company}</div>
+           <div class="item-name">${item.item_name}/div>
+            <div class="price">
+               <span class="current-price">Rs ${item.current_price}</span>
+               <span class="original-price">Rs ${item.original_price}</span>
+               <span class="discount">(${item.discount_percentage}% OFF)</span>
+           </div>
+               <button class="btn-add-bag" onclick="addToBag(${item.id})">Add To Bag</button>
+        </div>
+        
+        `;
+    });
+    itemsContainerElement.innerHTML = innerHTML;
+}
