bf0214356d9dc20267671fce356dfbcad4708909
Chaitanya <chaitanyaasole@gmail.com>
2026-08-22 10:26:50 +0530

    course_kg_coding

--- interviewQuestions/coding_questions/05_anagram.js
+++ interviewQuestions/coding_questions/05_anagram.js
@@ -1,15 +1,28 @@
-let str1 = "hello";
-let str2 = "llohe";
+const findVowels = (str) => {
+  let count = 0;
+  const vowels = ["a", "e", "i", "o", "u"];
+  console.log(Array.isArray(vowels)); // true
 
-function check(s, t) {
-  let first = s.split("").sort().join("");
-  let second = s.split("").sort().join("");
-
-  if (first === second) {
-    return true;
+  for (let char of str.toLowerCase()) {
+    if (vowels.includes(char)) {
+      count++;
+    }
   }
-}
+  return count;
+};
 
-if (check(str1, str2)) {
-  console.log("Anagram");
-}
+// let str1 = "hello";
+// let str2 = "llohe";
+
+// function check(s, t) {
+//   let first = s.split("").sort().join("");
+//   let second = s.split("").sort().join("");
+
+//   if (first === second) {
+//     return true;
+//   }
+// }
+
+// if (check(str1, str2)) {
+//   console.log("Anagram");
+// }
